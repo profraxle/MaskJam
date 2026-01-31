a30419564cb65e5f00680a764302f3fe9c82c98a
GEORGE FYLES <2100707@uad.ac.uk>
2026-01-31 18:40:26 +0000

    Pause menu

---
+++ Assets/Scripts/HUD/PauseMenuScript.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    [SerializeField]
+    string SceneToRestart;
+
+    [SerializeField]
+    InputActionReference PauseAction;
+
+    public bool Paused = false;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        PauseAction.action.Enable();
+        GetComponent<CanvasGroup>().alpha = 0;
+    }
+    void TogglePause()
+    {
+        Paused = !Paused;
+        if (Paused)
+        {
+            GetComponent<CanvasGroup>().alpha = 1;
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            GetComponent<CanvasGroup>().alpha = 0;
+            Cursor.lockState = CursorLockMode.Locked;
+            Time.timeScale = 1f;
+        }
+    }
+
+    private void Update()
+    {
+        if (PauseAction.action.triggered)
+        {
+            TogglePause();
+        }
+    }
+
+    public void OnContinueClicked()
+    {
+        TogglePause();
+    }
+
+    public void OnRestartClicked()
+    {
+        SceneManager.LoadScene(SceneToRestart);
+    }
+
+    public void OnMainMenuClicked()
+    {
+        SceneManager.LoadScene("SCN_MainMenu");
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+    }
+}
