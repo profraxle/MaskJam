c130e733335c2d5ab74ec07b4bf86cf2494fd1c6
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2026-01-31 18:51:22 +0000

    Doors can be opened and closed with or without delay

---
+++ Assets/Scripts/Scenery/Door.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private bool isOpen;
+
+    [SerializeField]
+    private float bottomPos;
+    [SerializeField]
+    private float topPos;
+
+    [SerializeField]
+    private float openSpeed = 1f;
+
+    [SerializeField]
+    private float openDelay = 0f;
+    [SerializeField]
+    private GameObject doorMesh;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (openDelay == 0) {
+            if (!isOpen)
+            {
+                if (doorMesh.transform.position.y > bottomPos)
+                {
+                    doorMesh.transform.position =
+                        doorMesh.transform.position - new Vector3(0, openSpeed * Time.fixedDeltaTime, 0);
+                }
+
+                if (doorMesh.transform.position.y < bottomPos)
+                {
+                    doorMesh.transform.position =
+                        new Vector3(doorMesh.transform.position.x, bottomPos, doorMesh.transform.position.z);
+                }
+            }
+            else
+            {
+                if (doorMesh.transform.position.y < topPos)
+                {
+                    doorMesh.transform.position =
+                        doorMesh.transform.position + new Vector3(0, openSpeed * Time.fixedDeltaTime, 0);
+                }
+
+                if (doorMesh.transform.position.y > topPos)
+                {
+                    doorMesh.transform.position =
+                        new Vector3(doorMesh.transform.position.x, topPos, doorMesh.transform.position.z);
+                }
+            }
+        }
+        else
+        {
+            openDelay -= Time.fixedDeltaTime;
+            if (openDelay < 0)
+            {
+                openDelay = 0;
+            }
+        }
+    }
+    
+    public void SetOpen(bool open)
+    {
+        isOpen = open;
+    }
+    
+    public void SetDelay(float delay)
+    {
+        openDelay = delay;
+    }
+}
