fe2674079256c521e53bd93d529553abc20798f8
AbertayFraxle <2100629@uad.ac.uk>
2026-02-01 15:57:52 +0000

    LIGHT

--- Assets/Scripts/Scenery/Door.cs
+++ Assets/Scripts/Scenery/Door.cs
@@ -24,7 +24,7 @@ public class Door : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (openDelay == 0) {
             if (!isOpen)
